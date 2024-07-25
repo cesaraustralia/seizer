22a042f0881964aa73caadf4a2b79b45a478d0b6
Alex Slavenko <alex.slavenko1@gmail.com>
2024-07-25 13:20:44 +1000

    Fix to cesar_save()

--- R/cesar_save.R
+++ R/cesar_save.R
@@ -5,7 +5,7 @@
 #' @param filename File name to create on disk, png unless otherwise specified
 #' @param plot Plot to save, defaults to last plot displayed.
 #' @param path Path of the directory to save plot to: path and filename are combined to create the fully qualified file name. Defaults to the working directory.
-#' @param preset Passes pre-determined values to the arguments \code{scale}, \code{width}, \code{height} and \code{dpi} based on the type of output the visualisation is required for. Accepts one of \code{c("twitter", "web", "linkedin", "facebook", "print")}. Deafults to \code{"print"}.
+#' @param preset Passes pre-determined values to the arguments \code{scale}, \code{width}, \code{height} and \code{dpi} based on the type of output the visualisation is required for. Accepts one of \code{c("twitter", "web", "linkedin", "facebook", "print")} and overwrites user-provided values. If \code{NULL} (default) inherits \code{ggplot2::ggave} default values.
 #' @param logo if \code{TRUE} add Cesar logo at top right. Defaults to \code{FALSE}
 #' @param ... additional arguments to that pass to ggsave
 #'
@@ -31,66 +31,76 @@
 cesar_save <- function(filename,
                        plot = last_plot(),
                        path = NULL,
-                       preset = "print",
+                       preset = NULL,
                        logo = FALSE,
+                       scale = 1,
+                       width = NA,
+                       height = NA,
+                       units = c("in", "cm", "mm", "px"),
+                       dpi = 300,
                        ...){
 
   if(!grepl("png$|pdf$|svg$|jpeg$|tiff$", filename))
     filename <- paste0(filename, ".png")
 
-  if(!preset %in% c("twitter", "web", "linkedin", "facebook", "print")){
-    warning("unknown preset provided; defaulting to print")
-    scale = 1
-    height = 5.625
-    width = 10
-    dpi = 300
+  if(!is.null(preset)){
+    if(!preset %in% c("twitter", "web", "linkedin", "facebook", "print")){
+      warning("unknown preset provided; defaulting to print")
+      scale = 1
+      height = 5.625
+      width = 10
+      dpi = 300
 
-    showtext::showtext_opts(dpi = 300)
-  }
+      showtext::showtext_opts(dpi = 300)
+    }
 
-  if(preset == "twitter"){
-    scale = 1
-    height = 12.5
-    width = 22.2
-    dpi = 72
+    if(preset == "twitter"){
+      scale = 1
+      height = 12.5
+      width = 22.2
+      dpi = 72
 
-    showtext::showtext_opts(dpi = 72)
-  }
+      showtext::showtext_opts(dpi = 72)
+    }
 
-  if(preset == "web"){
-    scale = .5
-    height = 18.5
-    width = 27.7
-    dpi = 72
+    if(preset == "web"){
+      scale = .5
+      height = 18.5
+      width = 27.7
+      dpi = 72
 
-    showtext::showtext_opts(dpi = 72)
-  }
+      showtext::showtext_opts(dpi = 72)
+    }
 
-  if(preset == "linkedin"){
-    scale = 1
-    height = 8.7
-    width = 16.7
-    dpi = 72
+    if(preset == "linkedin"){
+      scale = 1
+      height = 8.7
+      width = 16.7
+      dpi = 72
 
-    showtext::showtext_opts(dpi = 72)
-  }
+      showtext::showtext_opts(dpi = 72)
+    }
 
-  if(preset == "facebook"){
-    scale = 1
-    height = 8.75
-    width = 16.7
-    dpi = 72
+    if(preset == "facebook"){
+      scale = 1
+      height = 8.75
+      width = 16.7
+      dpi = 72
 
-    showtext::showtext_opts(dpi = 72)
-  }
+      showtext::showtext_opts(dpi = 72)
+    }
 
-  if(preset == "print"){
-    scale = 1
-    height = 5.625
-    width = 10
-    dpi = 300
+    if(preset == "print"){
+      scale = 1
+      height = 5.625
+      width = 10
+      dpi = 300
 
-    showtext::showtext_opts(dpi = 300)
+      showtext::showtext_opts(dpi = 300)
+    }
+  } else{
+
+    showtext::showtext_opts(dpi = dpi)
   }
 
   if(logo){
@@ -105,6 +115,7 @@ cesar_save <- function(filename,
                     scale = scale,
                     width = width,
                     height = height,
+                    units = units,
                     dpi = dpi,
                     ...)
 
@@ -118,6 +129,7 @@ cesar_save <- function(filename,
                     scale = scale,
                     width = width,
                     height = height,
+                    units = units,
                     dpi = dpi,
                     ...)
 
